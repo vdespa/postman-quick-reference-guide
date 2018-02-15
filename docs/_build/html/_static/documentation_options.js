5ebb8cc8c0f0c512e6cb931d74b851201cea9cd4
Valentin Despa <valentin@vdespa.com>
2018-02-15 19:22:00 +0100

    First draft update

--- docs/_build/html/_static/documentation_options.js
+++ docs/_build/html/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: 'February 2018',
+    VERSION: 'Preview Release - February 2018',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
